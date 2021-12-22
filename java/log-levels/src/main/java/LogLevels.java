0a9247058259ec05d71de8bf6eda6b6a32608bad
jordan-s99 <jj1861@outlook.com>
2021-12-22 17:42:32 +0000

    log-levels exercise

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,16 +1,32 @@
+import java.util.Locale;
+
 public class LogLevels {
     
     public static String message(String logLine) {
         String[] split = logLine.split(": ", 2);
         String message = split[1];
-        return message;
+        return message.trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        // get log level only i.e. split at ]
+        // remove [ ]
+        // turn to lower case
+        String[] split = logLine.split("]", 2);
+        String level = split[0];
+//        level = level.substring(0, level.indexOf("[") -1);
+//        level = level.substring(0, level.length() -1);
+       level = level.replaceAll("\\[", "").replaceAll("//]", "").toLowerCase();
+        return level;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+      // put the message first, and the log level after it in parenthesis
+        String[] split = logLine.split(": ", 2);
+        String level = split[0];
+        level  = level.replaceAll("\\[", "").replaceAll("\\]", "").toLowerCase();
+        String message = split[1];
+        String reformatted = message.trim() + " (" + level + ")";
+        return reformatted;
     }
 }
